e50eb23830a6f54570c9b884a79fd67c60258a4f
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2026-04-17 20:03:45 -0300

    Merged awake

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -23,10 +23,6 @@ namespace Controllers
         private void Awake()
         {
             GetComponent<HealthComponent>().OnDeath += () => Destroy(gameObject);
-        }
-
-        private void Awake()
-        {
             playerTransform = GameManagerScript.Instance.Player;
         }
 
